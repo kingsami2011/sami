f65084d62102b10b81ed680cabd04bfa340b9ef0
app_ef002fbb <app_ef002fbb@ip-10-169-1-220.ec2.internal>
2014-04-10 13:20:33 +0000

    AndroidKickstartR generation

---
+++ sami/src/com/sami/myapp/sami.java
@@ -0,0 +1,25 @@
+
+package com.sami.myapp;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class sami
+    extends SherlockFragmentActivity
+{
+
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
